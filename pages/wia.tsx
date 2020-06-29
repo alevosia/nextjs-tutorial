642f568a9abff1ad7ddd5d497351e730ce081d19
Alexander Paul <alexanderpaul.marinas@gmail.com>
2020-06-30 03:00:57 +0800

    Fetch WIA posts

--- pages/wia.tsx
+++ pages/wia.tsx
@@ -1,18 +1,42 @@
+import { useState, useEffect } from 'react'
 import { GetServerSideProps } from 'next'
 import { ParsedUrlQuery } from 'querystring'
+import axios from 'axios'
 
 interface Props {
     url: string
     query: ParsedUrlQuery
+    posts: any[]
 }
 
-const Wia = ({ url, query }: Props) => {
+async function fetchPosts() {
+    console.log('Fetching Posts')
+
+    const response = await axios.get('https://api.womaninaction.ph/posts')
+    const posts = response.data
+
+    return posts
+}
+
+const Wia: React.FC<Props> = ({ url, query, posts }) => {
     console.log('WIA Rendering')
+
     return (
         <div>
             <a href={url}>{url}</a>
             <br />
             <span>{query.name}</span>
+            {
+                posts.length > 0 
+                    ? posts.map((post) => (
+                        <div key={post.id}>
+                            <h2>{post.title}</h2>
+                            <span>{post.author_name}</span>
+                            <p>{post.content.substring(0, 150).concat('...')}</p>
+                        </div>
+                    ))
+                    : 'Fetching posts...'
+            }
         </div>
     )
 }
@@ -24,10 +48,15 @@ export default Wia
 // runs on the server for every request during runtime
 export const getServerSideProps: GetServerSideProps<Props> = async(context) => {
     console.log('GetServerSideProps')
+
+    const posts = await fetchPosts()
+
+    console.log('Posts fetched by server')
     return {
         props: {
             url: context.req.url,
-            query: context.query
+            query: context.query,
+            posts
         }
     }
 }
